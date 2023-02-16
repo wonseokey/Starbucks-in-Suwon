d75f45a3329c576c2937498c40d07d2d88aaaec9
WONSEOK LEE <67875602+wonseokey@users.noreply.github.com>
2023-02-16 12:45:39 +0900

    matplotlib 사용해서 시각화
    matplotlib.pyplot 사용해서 시각화. 
    점포당 인구수가 많은 곳을 선택해서 스타벅스 본사에 신규 매장 입점을 제안가능

---
+++ starbucks_suwon.py
@@ -0,0 +1,40 @@
+import csv
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] ='Malgun Gothic'
+
+plt.rcParams['axes.unicode_minus'] =False 
+plt.rc('font', size=7)
+
+f = open("C:\Lesson_Python\starbucks_suwon\data.csv")
+data = csv.reader(f) # csv파일은 콤마로 구분
+next(data) # 타이틀 안보이게 함
+
+dong = ["영통동",
+    "원천동",
+    "망포동",
+    "신동",
+    "서둔동",
+    "세류동",
+    "권선동",
+    "평동",
+    "금곡동",
+    "송죽동",
+    "정자동",
+    "율천동",
+    "파장동",
+    "연무동",
+    "인계동",
+    "우만동",
+    "매산동"
+    ]
+
+aa =[]
+for row in data :
+    if (row[4] != "") :
+        aa.append(float(row[4]))
+
+plt.title("수원시 행정구역 별 스타벅스 지점에 할당된 인구")
+plt.plot(dong, aa)
+plt.scatter(dong, aa)
+plt.show()
